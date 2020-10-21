1afd46dc30904345124b23d4aed4676080097669
eggeek <eggeek.me@gmail.com>
2020-10-21 17:04:40 +1100

    update testcases

--- warthog/testcases/test-jlimit.hpp
+++ warthog/testcases/test-jlimit.hpp
@@ -19,40 +19,115 @@ struct node {
   int x, y;
 };
 
-const double eps = 1e-2;
+const double eps = 1e-4;
 namespace w=warthog;
 
-TEST_CASE("jlimit-8") {
-  string mpath = "./testcases/maps/square-8.map";
-  w::gridmap map(mpath.c_str());
-  w::jps_expansion_policy_prune exp(&map);
-  w::octile_heuristic heuristic(map.width(), map.height());
-  w::flexible_astar<
-    w::octile_heuristic, 
-    w::jps_expansion_policy_prune> astar(&heuristic, &exp);
-  astar.set_verbose(false);
+void run(w::gridmap& map, vector<node>& s, vector<node>& t, bool verbose=false) {
 
+  long long tot0, tot1, exp0, exp1, gen0, gen1, touch0, touch1;
+  double time0, time1;
+    w::jps_expansion_policy_prune jps0(&map);
+    w::octile_heuristic heuristic0(map.width(), map.height());
+    w::flexible_astar<
+      w::octile_heuristic,
+      w::jps_expansion_policy_prune> astar0(&heuristic0, &jps0);
 
-  vector<tuple<node, node, double, bool, bool>> queries = {
-    {{1, 3}, {9, 5}, 11.414, false, false},
-    {{1, 3}, {9, 5}, 11.414, false, true},
+    jps0.get_locator()->jprune = true;
+    jps0.get_locator()->gprune = true;
+    astar0.set_verbose(verbose);
+
+    w::jps_expansion_policy_prune jps1(&map);
+    w::octile_heuristic heuristic1(map.width(), map.height());
+    w::flexible_astar<
+      w::octile_heuristic,
+      w::jps_expansion_policy_prune> astar1(&heuristic1, &jps1);
+
+    jps1.get_locator()->jprune = false;
+    jps1.get_locator()->gprune = false;
+    astar1.set_verbose(verbose);
+
+    tot0 = tot1 = exp0 = exp1 = gen0 = gen1 = touch0 = touch1 = 0;
+    time0 = time1 = 0;
+    for (int i=0; i<(int)s.size(); i++) {
+      jps0.get_locator()->jpruner->scan_cnt = 0;
+
+      int sid = s[i].y * map.header_width() + s[i].x;
+      int tid = t[i].y * map.header_width() + t[i].x;
+
+      double len = astar0.get_length(map.to_padded_id(sid), map.to_padded_id(tid));
+      exp0 += astar0.get_nodes_expanded();
+      gen0 += astar0.get_nodes_generated();
+      touch0 += astar0.get_nodes_touched();
+      time0 += astar0.get_search_time();
+      tot0 += jps0.get_locator()->jpruner->scan_cnt;
+
+      int e0 = astar0.get_nodes_expanded();
+
+      jps1.get_locator()->jpruner->scan_cnt = 0;
+
+      double len2 = astar1.get_length(map.to_padded_id(sid), map.to_padded_id(tid));
+      exp1 += astar1.get_nodes_expanded();
+      gen1 += astar1.get_nodes_generated();
+      touch1 += astar1.get_nodes_touched();
+      time1 += astar1.get_search_time();
+      tot1 += jps1.get_locator()->jpruner->scan_cnt;
+
+      int e1 = astar1.get_nodes_expanded();
+
+      if (fabs(len - len2) > eps || e0 > e1) {
+        cerr << "1\t" << map.filename() << "\t" << map.header_height()
+             << "\t" << map.header_width() << "\t" << s[i].x << "\t" << s[i].y
+             << "\t" << t[i].x << "\t" << t[i].y << "\t" << len2 << endl;
+        cerr << "len: " << len << ", len2: " << len2 << endl;
+      }
+
+      REQUIRE(e0 <= e1);
+      REQUIRE(fabs(len - len2) < eps);
+    }
+    cout << "---------------------------" << endl;
+    cout << "algo\texp\tgen\ttouch\ttime\tscan\n" << endl;
+    cout << "jlimit\t" << exp0 << "\t" << gen0 << "\t" << touch0 << "\t" << time0 << "\t" << tot0 << endl;
+    cout << "normal\t" << exp1 << "\t" << gen1 << "\t" << touch1 << "\t" << time1 << "\t" << tot1 << endl;
+
+}
+
+TEST_CASE("jlimit-random") {
+  vector<string> queries = {
+    "./testcases/random-256.query",
+    "./testcases/random-512.query",
+    "./testcases/random-1024.query",
   };
+  vector<node> s, t;
+  string mpath;
+  int num;
+  for (const auto& qpath: queries) {
+    ifstream file(qpath);
+    file >> mpath >> num;
+    w::gridmap map(mpath.c_str());
+    s.resize(num), t.resize(num);
+    for (int i=0; i<num; i++) {
+      int sx, sy, tx, ty;
+      file >> sx >> sy >> tx >> ty;
+      s[i] = node{sx, sy};
+      t[i] = node{tx, ty};
+    }
+    run(map, s, t);
+  }
+}
 
-
-  for (const auto& q: queries) {
-    node s = get<0>(q), t = get<1>(q);
-    double ans = get<2>(q);
-    bool gprune = get<3>(q), jlimit = get<4>(q);
-
-    exp.get_locator()->gprune = gprune;
-    exp.get_locator()->jprune = jlimit;
-    exp.get_locator()->jpruner->scan_cnt = 0;
-
-    int sid = s.y * map.header_width() + s.x;
-    int gid = t.y * map.header_width() + t.x;
-    double len = astar.get_length(map.to_padded_id(sid), map.to_padded_id(gid));
-    REQUIRE(fabs(len - ans) < eps);
-    cout << exp.get_locator()->jpruner->scan_cnt << endl;
+TEST_CASE("jlimit-maxscan") {
+  vector<string> mpaths = {
+    "./testcases/maps/maxscan-128.map",
+    "./testcases/maps/maxscan-256.map",
+    "./testcases/maps/maxscan-512.map",
+    "./testcases/maps/maxscan-1024.map"
+  };
+  for (const auto& mpath: mpaths) {
+    w::gridmap map(mpath.c_str());
+    int l = map.header_height();
+    vector<node> s = {{1, l / 2}};
+    vector<node> t = {{l-2, l / 2}};
+    run(map, s, t);
   }
 }
 
